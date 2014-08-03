edf265f56b888cfae60f273e4e3e4484b85d6e45
Tyler Smith <tylsmith@gmail.com>
2014-08-02 18:45:14 -0700

    added accelerometer readout example

---
+++ accel-example/build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_app_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleAppInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { APP_INFO_CURRENT_STRUCT_VERSION_MAJOR, APP_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { APP_INFO_CURRENT_SDK_VERSION_MAJOR, APP_INFO_CURRENT_SDK_VERSION_MINOR },
+  .app_version = { 2, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Accel Discs",
+  .company = "Pebble Technology",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x3A, 0xA6, 0x43, 0xC5, 0xAC, 0xD4, 0x48, 0x97, 0x92, 0x13, 0x5E, 0x68, 0x12, 0xDA, 0x38, 0x57 },
+  .virtual_size = 0xb6b6
+};
